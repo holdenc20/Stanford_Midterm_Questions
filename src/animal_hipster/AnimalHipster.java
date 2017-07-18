3ba76f4cc768c0e6d5e0dcee990682eac8fbd3a6
DaveDLeague <DaveD@Leagues-iMac.attlocal.net>
2017-07-18 10:17:45 -0700

    inital

---
+++ src/animal_hipster/AnimalHipster.java
@@ -0,0 +1,14 @@
+package animal_hipster;
+
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AnimalHipster {
+
+	public AbstractCollection<String> findAnimalHipsters(HashMap<String, ArrayList<String>> network,
+			HashMap<String, String> favoriteAnimals) {
+		return null;
+	}
+
+}
---
+++ src/oncogene/OncogeneDetector.java
@@ -0,0 +1,12 @@
+package oncogene;
+
+import java.util.ArrayList;
+
+public class OncogeneDetector {
+
+	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequences, String string) {
+		
+		return false;
+	}
+
+}
